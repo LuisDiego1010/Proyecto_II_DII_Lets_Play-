743b88f06ee3d0ee62ed593addacc24ce67fcfcf
Carlos Akion Garro Campos <carloscamp1008@estudiantec.cr>
2021-05-09 16:31:43 -0600

    Update Window

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -10,13 +10,13 @@ using namespace sf;
 using namespace std;
 int main()
 {
-    RenderWindow window(sf::VideoMode(1000, 1000), "Let's Play");
+    RenderWindow window(sf::VideoMode(1200, 1400), "Let's Play");
     Texture BpGame;
     Texture PuzzleGame;
-    if(!BpGame.loadFromFile("/home/garroakion/Desktop/TEC/src/cpp/bg.png")){
+    if(!BpGame.loadFromFile("/home/garroakion/Desktop/LetsPlayProject/src/cpp/images/BPGAME.png")){
         cout<<"No se puede cargar la imagen";
     }
-    if(!PuzzleGame.loadFromFile("/home/garroakion/Desktop/TEC/src/cpp/bg.png")){
+    if(!PuzzleGame.loadFromFile("/home/garroakion/Desktop/LetsPlayProject/src/cpp/images/Genetic Puzzle.png")){
         cout<<"No se puede cargar la imagen";
     }
 
@@ -28,23 +28,40 @@ int main()
 
     bpSprite.setOrigin(-80,-100 );
     //Vector2u size = texture.getSize();
-    puzzleSprite.setOrigin(-80,-500 );
+    puzzleSprite.setOrigin(-650,-100 );
 
 
     while (window.isOpen())
     {
         Event event;
+        auto mouse_pos = sf::Mouse::getPosition(window); // Mouse position relative to the window
+        auto translated_pos = window.mapPixelToCoords(mouse_pos); // Mouse position translated into world coordinates
         while (window.pollEvent(event))
         {
-            if (event.type == Event::Closed)
+            if (event.type == Event::Closed){
                 window.close();
+            }
+
+
+            else if(bpSprite.getGlobalBounds().contains(translated_pos)){
+                if (event.type == Event::MouseButtonPressed){
+
+                }
+            }
+            else if(puzzleSprite.getGlobalBounds().contains(translated_pos)){
+                if (event.type == Event::MouseButtonPressed){
+
+                }
+            }
+
+
         }
         window.clear(Color(255, 255, 255));
         //window.clear(Color(35, 181, 184));
         window.draw(bpSprite);
         window.draw(puzzleSprite);
         window.display();
-        cout<<"Hola";
+
     }
 
     return 0;
