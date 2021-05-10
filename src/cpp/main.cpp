3fb6d9a09cce0b2121c9a6779ba61222af7c441d
LuisDiego1010 <guisogarcia1010@estudiantec.cr>
2021-05-09 19:29:15 -0600

    Create a Genetic Puzzle window

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -7,7 +7,7 @@
 #include <filesystem>
 
 void windowBPGame();
-
+void windowGeneticPuzzle();
 using namespace sf;
 using namespace std;
 int main() {
@@ -45,7 +45,7 @@ int main() {
                 }
             } else if (puzzleSprite.getGlobalBounds().contains(translated_pos)) {
                 if (event.type == Event::MouseButtonPressed) {
-
+                    windowGeneticPuzzle();
                 }
             }
         }
@@ -60,7 +60,7 @@ int main() {
 }
 
 void windowBPGame() {
-    RenderWindow window(sf::VideoMode(1200, 1400), "BP GAME");
+    RenderWindow window(sf::VideoMode(1600, 1400), "BP GAME");
     Texture field;
     Texture obstacule1;
     Texture obstacule2;
@@ -68,13 +68,12 @@ void windowBPGame() {
     if (!field.loadFromFile(
             "src/images/sprfield.jpg")) {
     }if (!obstacule1.loadFromFile(
-            "src/images/Player1.png")) {
+            "src/images/sprPlayer1.png")) {
     }if (!obstacule2.loadFromFile(
-            "src/images/Player2.png")) {
+            "src/images/sprPlayer2.png")) {
     }if (!ball.loadFromFile(
             "src/images/sprball.png")) {
     }
-
 
     Sprite fieldSprite;
     fieldSprite.setTexture(field);
@@ -109,5 +108,20 @@ void windowBPGame() {
         window.draw(obstacule2Sprite);
         window.draw(ballSprite);
         window.display();
+    }
+}
+void windowGeneticPuzzle(){
+    RenderWindow window(sf::VideoMode(1200, 1400), "GENETIC PUZZLE");
+        while (window.isOpen()) {
+            Event event;
+            auto mouse_pos = sf::Mouse::getPosition(window); // Mouse position relative to the window
+            auto translated_pos = window.mapPixelToCoords(mouse_pos); // Mouse position translated into world coordinates
+            while (window.pollEvent(event)) {
+                if (event.type == Event::Closed) {
+                    window.close();
+                }
+            }
+            window.clear(Color(255, 255, 255));
+            window.display();
     }
 }
