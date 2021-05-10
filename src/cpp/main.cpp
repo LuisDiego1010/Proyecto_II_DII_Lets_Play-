1c65ad406b5e136e6983c4688eed60d5eadf2253
LuisDiego1010 <guisogarcia1010@estudiantec.cr>
2021-05-09 19:21:20 -0600

    Create a BPGame window

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,10 +6,11 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <filesystem>
 
+void windowBPGame();
+
 using namespace sf;
 using namespace std;
-int main()
-{
+int main() {
     RenderWindow window(sf::VideoMode(1200, 1400), "Let's Play");
     Texture BpGame;
     Texture PuzzleGame;
@@ -26,43 +27,87 @@ int main()
     Sprite puzzleSprite;
     puzzleSprite.setTexture(PuzzleGame);
 
-    bpSprite.setOrigin(-80,-100 );
+    bpSprite.setOrigin(-80, -100);
     //Vector2u size = texture.getSize();
     puzzleSprite.setOrigin(-650,-100 );
 
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         Event event;
         auto mouse_pos = sf::Mouse::getPosition(window); // Mouse position relative to the window
         auto translated_pos = window.mapPixelToCoords(mouse_pos); // Mouse position translated into world coordinates
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed){
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
                 window.close();
-            }
-
-
-            else if(bpSprite.getGlobalBounds().contains(translated_pos)){
-                if (event.type == Event::MouseButtonPressed){
+            } else if (bpSprite.getGlobalBounds().contains(translated_pos)) {
+                if (event.type == Event::MouseButtonPressed) {
+                    windowBPGame();
+                }
+            } else if (puzzleSprite.getGlobalBounds().contains(translated_pos)) {
+                if (event.type == Event::MouseButtonPressed) {
 
                 }
             }
-            else if(puzzleSprite.getGlobalBounds().contains(translated_pos)){
-                if (event.type == Event::MouseButtonPressed){
-
-                }
-            }
-
-
         }
         window.clear(Color(255, 255, 255));
         //window.clear(Color(35, 181, 184));
         window.draw(bpSprite);
         window.draw(puzzleSprite);
         window.display();
-
     }
 
     return 0;
+}
+
+void windowBPGame() {
+    RenderWindow window(sf::VideoMode(1200, 1400), "BP GAME");
+    Texture field;
+    Texture obstacule1;
+    Texture obstacule2;
+    Texture ball;
+    if (!field.loadFromFile(
+            "src/images/sprfield.jpg")) {
+    }if (!obstacule1.loadFromFile(
+            "src/images/Player1.png")) {
+    }if (!obstacule2.loadFromFile(
+            "src/images/Player2.png")) {
+    }if (!ball.loadFromFile(
+            "src/images/sprball.png")) {
+    }
+
+
+    Sprite fieldSprite;
+    fieldSprite.setTexture(field);
+
+    Sprite obstacule1Sprite;
+    obstacule1Sprite.setTexture(obstacule1);
+
+    Sprite obstacule2Sprite;
+    obstacule2Sprite.setTexture(obstacule2);
+
+    Sprite ballSprite;
+    ballSprite.setTexture(ball);
+
+    fieldSprite.setOrigin(-80, -100);
+    obstacule1Sprite.setOrigin(-650, -100);
+    obstacule2Sprite.setOrigin(-80, -100);
+    ballSprite.setOrigin(-650, -100);
+
+    while (window.isOpen()) {
+        Event event;
+        auto mouse_pos = sf::Mouse::getPosition(window); // Mouse position relative to the window
+        auto translated_pos = window.mapPixelToCoords(mouse_pos); // Mouse position translated into world coordinates
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(Color(255, 255, 255));
+        //window.clear(Color(35, 181, 184));
+        window.draw(fieldSprite);
+        window.draw(obstacule1Sprite);
+        window.draw(obstacule2Sprite);
+        window.draw(ballSprite);
+        window.display();
+    }
 }
